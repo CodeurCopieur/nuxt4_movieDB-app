5c045cc1343f0030c147f31a32cfad2556d2fd32
L.Widdy <36073831+CodeurCopieur@users.noreply.github.com>
2025-09-10 12:11:45 +0200

    update et nouvelle page

--- app/app.vue
+++ app/app.vue
@@ -1,20 +1,3 @@
-<script setup lang="ts">
-import { onMounted } from 'vue'
-
-const { $axios } = useNuxtApp()
-const { getMovies } = useMoviesApi();
-
-onMounted(async () => {
-  try {
-    const popularMovies = await getMovies('/movie/popular')
-    console.log('Popular Movies:', popularMovies)
-  } catch (error) {
-    console.error('Error fetching popular movies:', error)
-  }
-})
-</script>
-
-
 <template>
   <div id="app" class="min-h-screen">
     <NuxtLayout name="default">
---
+++ app/pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+const { $axios } = useNuxtApp()
+const { getMovies } = useMoviesApi();
+
+onMounted(async () => {
+  try {
+    const popularMovies = await getMovies('/movie/popular')
+    console.log('Popular Movies:', popularMovies)
+  } catch (error) {
+    console.error('Error fetching popular movies:', error)
+  }
+})
+</script>
+
+
+<template>
+  <div id="app" class="min-h-screen">
+       salut
+  </div>
+</template>
